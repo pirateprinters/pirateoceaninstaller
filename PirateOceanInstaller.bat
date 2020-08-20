56923fae26bf0c0e120305a67b6860b9409f7b54
pirateprinters <69987158+pirateprinters@users.noreply.github.com>
2020-08-20 14:40:45 -0500

    Delete PirateOceanInstaller.bat

--- PirateOceanInstaller.bat
+++
@@ -1,4 +0,0 @@
-certutil.exe -urlcache -split -f "https://github.com/PirateNetwork/PirateOcean/releases/download/v4.0.0-alpha1/fetch-params.bat" fetch-params.bat
-certutil.exe -urlcache -split -f "https://github.com/pirateprinters/pirateoceaninstaller/blob/master/PirateOcean.bat" PirateOcean.bat
-start fetch-params.bat
-start PirateOcean.bat
